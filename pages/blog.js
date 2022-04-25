efdbdee0caccdcf7715f08c2fc5bf991412457e2
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-04-25 13:10:54 +0530

    commit

--- pages/blog.js
+++ pages/blog.js
@@ -1,25 +1,56 @@
 import React,{useState,useEffect} from 'react'
 import styles from "../styles/blog.module.css"
 import Link from 'next/link';
+import InfiniteScroll from "react-infinite-scroll-component";
+import * as fs from "fs";
 const Blog = (props) => {
   const [blogs, setBlogs] = useState(props.allBlogs);
+  const [count,setCount] = useState(1)   
+  const fetchData = async () => {
+    let d = await fetch(`http://localhost:3000/api/blogs?count=${count+1}`)
+    setCount(count +1)  
+    let data = await d.json()
+      setBlogs(data)
+  };
   return (
     <div className= {styles.main}>
-        {blogs.map((blogitem)=>{
+      <InfiniteScroll
+  dataLength={blogs.length} //This is important field to render the next data
+  next={fetchData}
+  hasMore={props.allCount !== blogs.length}
+  loader={<h4>Loading...</h4>}
+  endMessage={
+    <p style={{ textAlign: 'center' }}>
+      <b>Yay! You have seen it all</b>
+    </p>
+  }
+>
+{blogs.map((blogitem)=>{
           return <div className={styles.blogItems} key={blogitem.slug}>
         <Link href={`/blogpost/${blogitem.slug}`}>
         <h2 className={styles.blogItemsh3}>{blogitem.title}</h2></Link>
-        <p>{blogitem.content.substring(0,140)}</p>
+        <p>{blogitem.content.substring(0,140)} ...</p>
       </div>
         })}
-      </div>
+    
+</InfiniteScroll>
+</div>
   );
 }
-export async function getServerSideProps(context){
-  let data= await fetch("http://localhost:3000/api/blogs")
-     let allBlogs = await data.json()
+        
+export async function getStaticProps(context){
+  let data= await fs.promises.readdir(`blogdata`)
+  let allCount = data.length;
+  let myFile
+  let allBlogs=[]
+  for (let index = 0; index <1; index++) {
+    const item = data[index];
+    myFile= await fs.promises.readFile(("blogdata/"+item),"utf-8")
+    allBlogs.push(JSON.parse(myFile))
+
+  }
 return{
-props : {allBlogs}
+props : {allBlogs,allCount}
 }
 }
 
